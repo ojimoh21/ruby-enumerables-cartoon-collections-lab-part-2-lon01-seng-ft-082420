985ac42302c5b81d2620da7c5e93fab8b9ee3602
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-05 12:52:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,6 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-
   planeteer_calls.any? {|call|
     call.length > 4 
   }
@@ -19,4 +18,6 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
+  
+  
 end
