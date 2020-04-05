fbb633eab3a074c27a18436854d38aa53a3b2b6e
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-05 13:02:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
-  valid_calls.find{ |call|
-    call == planeteer_calls
+  planeteer_calls.select{ |call|
+    call == valid_calls
   }
   
 end
