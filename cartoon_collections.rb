21bfa29d62a524ece520c15db51eb45f0f323a35
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-05 13:29:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find { |call| 
-    call == valid_calls.include?(call)
+    valid_calls.include?(call)
   }
 
 end
